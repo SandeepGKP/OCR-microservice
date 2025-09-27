f89005cbfe8052a1235d8dae59ec6ef3e134c4da
SandeepGKP <sandeepnishad638672@gmail.com>
2025-09-27 15:14:09 +0530

    committed

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const reportsRouter = require('./routes/reports');
 app.use(express.json());
 app.use('/api', reportsRouter);
 
-app.get('/', (req, res) => res.send('Server is working'));
+app.get('/api', (req, res) => res.send('Server is working'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
